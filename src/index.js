f4e2d87b82d59ff7ba5359639e83789c8fd4bf59
momobytestudio <akiarasakura081@gmail.com>
2026-09-23 12:16:59 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log("Kagami is starting...");
+const { Client, GatewayIntentBits } = require("discord.js");
+require("dotenv").config();
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.once("ready", () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
